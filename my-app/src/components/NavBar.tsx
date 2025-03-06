9802bab4064d8317f6b21cb27ae0b20693d4c4e0
Anant Verma <E088088@mcgladrey.rsm.net>
2025-03-07 02:05:46 +0530

    logo

--- my-app/src/components/NavBar.tsx
+++ my-app/src/components/NavBar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import logo from "../assets/GsynergyLogo.svg"
 const NavBar: React.FC = () => {
   return (
     <header className="navbar">
       <div className="logo-section">
         <img
-          src="https://via.placeholder.com/120x40?text=GSynergy+Logo"
+          src={logo}
           alt="GSynergy Logo"
           style={{ marginRight: 8 }}
         />
